0cb0891cdcd19f19bc99e732554a6e3ef55c5712
Kevin Youn <ysudream@gmail.com>
2017-12-21 17:47:26 +0900

    [1] Backend ID/PW form created (w/o mysql code)

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head></head>
+
+<BODY>
+	
+		<form>
+			ID<input type='text' name='identity'>
+			PASSWORD<input type='password' name='password'>
+			<input type='submit' value='submit'>
+		</form>
+
+	
+
+</BODY>
+
+</html>
