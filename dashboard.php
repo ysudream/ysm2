96651649295d979c83d15c1d8efaa5928ba368e9
Kevin Youn <ysudream@gmail.com>
2017-12-21 21:39:21 +0900

    [1] CSS for columns made. Class 'main content' incomplete.

---
+++ dashboard.php
@@ -0,0 +1,39 @@
+<html>
+
+<head></head>
+<body>
+
+	<div class='wrap' style="height:75%;width:100%;margin-top:80;">
+		<div class='display wrapper' style='border:1px solid;height:630px;width:1200px;margin:0 auto;'>
+			<div class='navigator' style='width:200px;height: 100%;margin: 0;float:left;display:block;'>
+				<ul>
+				<li>a</li>
+				<li>b</li>
+				<li>c</li>
+				</ul>
+				<!--bootstrap: vertical pill navigation-->
+			</div>
+		
+			<div class='main-content' style='float:left;display:block;margin-top:17px;'>
+				
+				<div class='current-ranking'>
+					<table>
+						<tr><th colspan="2">무위도식</th><th colspan="2">맥주권</th><th colspan="2">치킨권</th></tr>
+						<tr><td colspan="2">abc</td><td>abc</td><td>abc</td><td>tat</td><td>tdtd</td></tr>
+					</table>
+				</div>
+
+				<p><div class='latests-news'>
+
+				</div>
+
+				<div class='memo'>
+
+				</div>
+
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
